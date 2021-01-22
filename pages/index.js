e60850acd703c1be73a5bcc606fc6ff0fcc60331
Claudio Posado <claudio@posado.com.ar>
2021-01-22 18:03:09 -0300

    social icons added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Work from 'components/work/Work'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, fa } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+
 import DataWorks from 'data/works.json'
 // import styles from '../styles/Home.module.css'
 
@@ -21,6 +25,20 @@ export default function Home() {
         <h3>
           Senior Fullstack Developer
         </h3>
+
+        <div className="my-4">
+          <a className="p-1" href="https://github.com/cposado" target="blank" rel="noopener">
+            <FontAwesomeIcon icon={faGithub} className="icon-bars" size="lg"/>
+          </a>
+          <a className="p-1" href="https://www.linkedin.com/public-profile/in/claudio-posado-00491b4/" target="blank" rel="noopener">
+            <FontAwesomeIcon icon={faLinkedin} className="icon-bars" size="lg"/>
+          </a>
+
+          <a className="p-1" href="mailto:claudioposado@gmail.com" target="blank" rel="noopener">
+            <FontAwesomeIcon icon={faEnvelope} className="icon-bars" size="lg"/>
+          </a>
+
+        </div>
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-0">
